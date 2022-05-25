389dd2259037d26651f8187096d9729264382f04
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-25 09:25:22 -0500

    initial py solution

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,23 @@
 def to_roman(num):
-    # write your code here!
+    pattern = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
+    result=""
+    for x in pattern:
+        while num >= pattern[x]:
+            result += x
+            num-= pattern[x]
+
+    return result        
